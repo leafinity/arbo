60e17f143838c07aa9362c0f14a801aaa68da720
leafinity <radleaf@gmail.com>
2013-07-23 15:31:16 +0800

    board version.1

--- web/arbo.dart
+++ web/arbo.dart
@@ -5,20 +5,20 @@ void main() {
 }
 
 void createboard() {
-	DivElement cb = query("#chessboard");
+	DivElement cb = query(".chessboard");
 	for (int i = 0; i < 8; i++) {
 	  String horz = i == 0? "boardtop" : "";
-	  int top = i * 70;
+	  int top = i == 0? 0: ((i-1) * 70 + 71);
 	  for (int j = 0; j < 8; j++) {
 	  	String ver = j == 0? "boardleft" : "";
-	  	int left = j * 70;
+	  	int left = j == 0? 0: ((j-1) * 70 + 71);
 	  	if ((i == 4 && j ==4) || (i == 5 && j == 5))
-	  	  cb.nodes.add(new Element.html('<div id = "ch_child $horz $ver" style = "left : ${left}px; top : ${top}px;"><div id = "bc"></div></div>'));
+	  	  cb.nodes.add(new Element.html('<div class = "ch_child" style = "left : ${left}px; top : ${top}px;"><div class = "bc"></div></div>'));
 
 	  	if ((i == 4 && j ==5) || (i == 5 && j == 4))
-	  	  cb.nodes.add(new Element.html('<div id = "ch_child" style = "left : ${left}px; top : ${top}px;"><div id = "wc"></div></div>'));
+	  	  cb.nodes.add(new Element.html('<div class = "ch_child" style = "left : ${left}px; top : ${top}px;"><div class = "wc"></div></div>'));
 	  	else
-	  	  cb.nodes.add(new Element.html('<div id = "ch_child $horz $ver" style = "left : ${left}px; top : ${top}px;"></div>'));
+	  	  cb.nodes.add(new Element.html('<div class = "ch_child $horz $ver" style = "left : ${left}px; top : ${top}px;"></div>'));
 
 	  }
 	}
